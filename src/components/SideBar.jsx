1ac1be55bf3b162c1b62354931db210b2aa301b0
Olga <minaeva9@gmail.com>
2025-01-19 13:46:09 -0500

    SideBar added

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+function SideBar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul>
+        <li>Learning React</li>
+        <li>Mastering React</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default SideBar;
