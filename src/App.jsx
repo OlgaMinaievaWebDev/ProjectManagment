ed74b3e252b41486eef24b3056ad450490c21bd5
Olga <minaeva9@gmail.com>
2025-01-20 15:17:02 -0500

    switch between project and noProject components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
+import NewProject from "./components/NewProject";
 import SideBar from "./components/SideBar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProjectId: null,
+    }));
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else {
+    content = <NoProjectSelected onStartAddProject={handleAddProject} />;
+  }
+
   return (
-    <main className="h-screen my-8 flex gap-8 ">
-      <SideBar />
-      <NoProjectSelected />
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar onStartAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
