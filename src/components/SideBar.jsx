210f41c54a2026afd88cee475ab696f4fd8b14bc
Olga <minaeva9@gmail.com>
2025-01-31 16:52:51 -0500

    done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,7 +5,7 @@ function SideBar({
   selectedProjectId,
 }) {
   return (
-    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+    <aside className="w-1/3 h-auto px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
